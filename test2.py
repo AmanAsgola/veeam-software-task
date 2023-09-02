6284f707a6521f9fd94cb467996a9b961438bded
AmanAsgola <83400064+AmanAsgola@users.noreply.github.com>
2023-09-02 18:07:17 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -16,17 +16,20 @@ args = parser.parse_args()
 logging.basicConfig(filename=args.logfile, level=logging.INFO, format="%(asctime)s - %(message)s")
 
 def sync_folders(source,replica):
+	#listing all the directory in source and replica
 	source_items=os.listdir(source)
 	replica_items=os.listdir(replica)
 	for item in source_items:
 		source_path=os.path.join(source,item)
 		replica_path=os.path.join(replica,item)
 		if os.path.isfile(source_path):
+			#file creating operation
 			if item not in replica_items:
 				shutil.copy2(source_path, replica_path)
 				logging.info(f"created file {replica_path}")
 				print(f"created file {replica_path}")
 			else:
+				#file updating operation
 				source_mtime=os.path.getmtime(source_path)
 				replica_mtime=os.path.getmtime(replica_path)
 				if source_mtime>replica_mtime:
@@ -34,6 +37,7 @@ def sync_folders(source,replica):
 					logging.info(f"updated file {replica_path}")
 					print(f"updated file {replica_path}")
 		elif os.path.isdir(source_path):
+			#folder creating operation
 			if item not in replica_items:
 				os.mkdir(replica_path)
 				logging.info(f"created folder {replica_path}")
@@ -43,10 +47,12 @@ def sync_folders(source,replica):
 		source_path=os.path.join(source,item)
 		replica_path=os.path.join(replica,item)
 		if item not in source_items:
+			#removing operaion
 			if os.path.isfile(replica_path):
 				os.remove(replica_path)
 				logging.info(f"removed file {replica_path}")
 				print (f"removed file {replica_path}")
+			#folder removing operation
 			elif os.path.isdir(replica_path):
 				shutil.rmtree(replica_path)
 				logging.info(f"removed folder {replica_path}")
